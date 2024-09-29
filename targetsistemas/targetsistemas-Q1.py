a11f56d5c6255558f495ac72b2e87106a584be84
Evelyn Amélia <128506800+evellynamelia@users.noreply.github.com>
2024-09-29 19:03:22 -0300

    Add files via upload

---
+++ targetsistemas/targetsistemas-Q1.py
@@ -0,0 +1,20 @@
+"""
+1) Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0;
+Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+Imprimir(SOMA);
+Ao final do processamento, qual será o valor da variável SOMA?
+"""
+
+
+
+
+def targetsistemas(indice):
+    soma = 0
+    k = 0
+    while k < indice:
+        k += 1
+        soma += k
+    return soma
+
+resultado = targetsistemas(13)
+print(resultado)
